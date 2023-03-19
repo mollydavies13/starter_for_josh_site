b5ebbeab236aaf5be65425cc3563cb2e5dbaea81
mollydavies13 <monomolly13@gmail.com>
2023-03-19 13:47:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,21 +16,21 @@
         <h3 class="text-center">LATEST ARTICLES</h3>
             <div class="row">
                 <div class="col-md-4">
-                    <img class="img-fluid" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/image%202.jpg" alt="woman">
+                    <img class="img-fluid" src="http://170.187.231.66/~mesh7/wp-content/themes/starter_for_josh_site/images/image%202.jpg" alt="woman">
                         <h4 class="article-title">A PORTRAIT</h4>
                         <p class="date">Friday, November 10. 2030</p>
                         <p class=article-p>STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and  opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.</p>
                         <a class="readmore" href="#">CONTINUE READING</a>
                 </div>
             <div class="col-md-4">
-                    <img class="img-fluid" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/image%203.jpg" alt="game-model">
+                    <img class="img-fluid" src="http://170.187.231.66/~mesh7/wp-content/themes/starter_for_josh_site/images/image%203.jpg" alt="game-model">
                     <h4 class="article-title">A PORTRAIT</h4>
                     <p class="date">Friday, November 10. 2030</p>
                     <p class=article-p>STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and  opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.</p>
                     <a class="readmore" href="#">CONTINUE READING</a>
                 </div>
             <div class="col-md-4">
-                    <img class="img-fluid" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/image%204.jpg" alt="terrain">
+                    <img class="img-fluid" src="http://170.187.231.66/~mesh7/wp-content/themes/starter_for_josh_site/images/image%204.jpg" alt="terrain">
                     <h4 class="article-title">A PORTRAIT</h4>
                     <p class="date">Friday, November 10. 2030</p>
                     <p class=article-p>STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and  opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.</p>
