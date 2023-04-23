c4bfe6ee4bf2edeb42058d505a73c0b1a4f9c304
mollydavies13 <monomolly13@gmail.com>
2023-04-23 11:26:09 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,10 +3,13 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-          <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="whitkin">WHITKIN</span></a>
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+          <footer class="container-fluid text-center"><!-- div for the main image to stretch s to the edge of the page-->
         </div>
         <div class="col-md-6">
-            <p>Terms and Conditions</p>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?>
         </div>
     </div> <!-- row -->
 </div> <!-- container -->
